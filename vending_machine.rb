7147be35ba10c39430dae49fe6c2ef0ab30f09f6
Kuniharu Aramaki <sunnycountry99@gmail.com>
2014-04-10 15:05:52 +0900

    totalメソッドまで

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,14 +1,24 @@
 class VendingMachine
 
-  def initialize(value = 10)
-    @value = value
+  def initialize
+     @total = 0
   end　
 
   def insert(value)
-  	@value = value
-  	values = [10, 50]
-  	unless values.include?(value)
-  	 puts @value
-  	end 
+    @value = value
+    values = [10, 50, 100, 1000, 10000]
+    unless values.include?(value)
+      puts "#{@value} (not available)"
+    else 
+      puts "nil (OK)"
+      @total += @value
+    end
+      puts @total 
   end
+
+  def total
+    puts @total
+  end
+
 end
+
