840a75e82c0c4c095ee49a0b400f1f528ac90291
Kuniharu Aramaki <sunnycountry99@gmail.com>
2014-04-11 20:40:05 +0900

    一応動作だけ、ステップ１〜３

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,8 +1,11 @@
 class VendingMachine
   AVAILABLE_VALUES = [10, 50, 100, 500, 1000]
+  PURCHASABLE＿DRINK_NAMES = [:cola]
 
   def initialize
     @total = 0
+    @stock_table = {:cola => {:price=>120, :stock=>5}}
+    @sale_amount = 0
   end
 
   def insert(value)
@@ -12,7 +15,7 @@ class VendingMachine
       @total += value
     end 
   end
-
+    
   def total
     @total
   end
@@ -22,4 +25,32 @@ class VendingMachine
     @total = 0
     refund 
   end
+
+  def stock_info 
+    @stock_table
+  end
+
+  def purchasable?(drink_name)
+    PURCHASABLE＿DRINK_NAMES.include? drink_name
+  end 
+
+  def purchase(drink_name)
+    if purchasable? drink_name
+      if @total >= @stock_table[:cola][:price]
+       @sale_amount += @stock_table[:cola][:price]
+       @total -= @stock_table[:cola][:price]  
+       @stock_table[:cola][:stock] = @stock_table[:cola][:stock] - 1
+       true
+      else
+        false
+      end
+    else
+      false  
+    end
+  end  
+
+  def sale_amount
+    @sale_amount 
+  end
+
 end
