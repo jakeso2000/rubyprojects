6a757404584f47ac5b2ce03cd74f243fdddaf0d2
Kuniharu Aramaki <sunnycountry99@gmail.com>
2014-04-16 12:07:36 +0900

    purchaseを短く

--- vending_machine.rb
+++ vending_machine.rb
@@ -32,21 +32,15 @@ class VendingMachine
 
   def purchasable?(drink_name)
     PURCHASABLE＿DRINK_NAMES.include? drink_name
+    @total >= @stock_table[:cola][:price]   
   end 
 
   def purchase(drink_name)
-    if purchasable? drink_name
-      if @total >= @stock_table[:cola][:price]
-       @sale_amount += @stock_table[:cola][:price]
-       @total -= @stock_table[:cola][:price]  
-       @stock_table[:cola][:stock] = @stock_table[:cola][:stock] - 1
-       true
-      else
-        false
-      end
-    else
-      false  
-    end
+    return false unless purchasable? drink_name
+      @sale_amount += @stock_table[:cola][:price]
+      @stock_table[:cola][:stock] = @stock_table[:cola][:stock] - 1
+      @total -= @stock_table[:cola][:price]  
+      true  
   end  
 
   def sale_amount
